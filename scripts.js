6317cc687a79b65fbc7434845dc7e571d94ad5f8
Owen Powell <opowell2899@gmail.com>
2021-05-16 13:08:40 -0400

    Added text input field for adding tasks

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 function addTask() {
+    let input = prompt("What is your task?");
     let ul = document.getElementById('taskList');
     let li = document.createElement("li");
-    li.appendChild(document.createTextNode("test"));
+    li.appendChild(document.createTextNode(input));
     ul.appendChild(li);
 }
